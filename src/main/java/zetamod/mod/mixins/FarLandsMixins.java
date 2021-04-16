4b83440b9e18fa4db6e36941032579f12f44b49f
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-04-16 15:54:55 -0400

    Moved farlands files into a seperate subclass of GeneralManager.class

--- src/main/java/zetamod/mod/mixins/FarLandsMixins.java
+++ src/main/java/zetamod/mod/mixins/FarLandsMixins.java
@@ -14,6 +14,7 @@ public class FarLandsMixins {
 	@Overwrite
 	public static double maintainPrecision(double d) {
 
-		return GeneralManager.maintainPrecisionManageable(d);
+		return GeneralManager.FarLandsManager.
+				maintainPrecisionManageable(d);
 	}
 }
