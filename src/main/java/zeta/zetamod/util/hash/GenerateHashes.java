c0bc99764884ee1f34f6a6ae23c2fe90f01be62c
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-04-26 18:44:49 -0400

    modified hash generator slightly

--- src/main/java/zeta/zetamod/util/hash/GenerateHashes.java
+++ src/main/java/zeta/zetamod/util/hash/GenerateHashes.java
@@ -1,15 +1,9 @@
 package zeta.zetamod.util.hash;
 
+import java.util.Arrays;
+
 public class GenerateHashes {
     public static void main(String... args) {
-        byte largestHashInitialValue = Byte.MAX_VALUE;
-        String[] trinaryHashes = new String[largestHashInitialValue];
-        int i = 0;
-        System.out.println("Mind us while we count to " + largestHashInitialValue);
-        while (i < largestHashInitialValue) {
-            trinaryHashes[i] = new TrinaryHash(i).hash();
-            System.out.println(trinaryHashes[i]);
-            i++;
-        }
+        System.out.println(Arrays.toString(TrinaryHash.genRandomHash()));
     }
 }
