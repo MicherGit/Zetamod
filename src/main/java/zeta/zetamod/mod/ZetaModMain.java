e44254c4bccf78112ca1b6b9c5850953945f9eff
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-04-27 11:09:08 -0400

    Fixed irritatering bug and push (first and only) release candidate

--- src/main/java/zeta/zetamod/mod/ZetaModMain.java
+++ src/main/java/zeta/zetamod/mod/ZetaModMain.java
@@ -35,7 +35,7 @@ public class ZetaModMain implements ModInitializer {
 	public static boolean MOD_DEV = false;
 	public static int MOD_DEV_V =
 
-			42
+			43
 			;
 	public static final byte V_TYPE = 0;
 	public static String MOD_VERSION = MOD_MAJOR + "." + MOD_MINOR + "." + MOD_BF;
@@ -55,7 +55,7 @@ public class ZetaModMain implements ModInitializer {
 	public void onInitialize() {
 		//TrinaryHash.checkHash();
 		log(Level.INFO, "Initializing");
-		MOD_VERSION = MOD_VERSION + MOD_DEV_V;
+		MOD_VERSION = MOD_VERSION + "." + MOD_DEV_V;
 		log(Level.INFO, "Mod version " + MOD_VERSION);
 		FarLandsCommand farLandsCommand = new FarLandsCommand();
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
