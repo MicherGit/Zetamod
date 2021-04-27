ab54c9d19ed1ec91e98962f36a52332786fdcb5a
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-04-27 09:38:33 -0400

    made keystone obtainable in survival

--- src/main/java/zeta/zetamod/mod/features/register/RegisterKeystone.java
+++ src/main/java/zeta/zetamod/mod/features/register/RegisterKeystone.java
@@ -1,5 +1,6 @@
 package zeta.zetamod.mod.features.register;
 
+import net.minecraft.item.Item;
 import zeta.zetamod.mod.ZetaModMain;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.ItemGroup;
@@ -10,9 +11,10 @@ import zeta.zetamod.mod.features.items.KeystoneItem;
 import java.util.Locale;
 
 public class RegisterKeystone {
+    public static Item KEYSTONE;
     public RegisterKeystone() {}
     public void registerItem() {
-        Registry.register(Registry.ITEM, new Identifier(ZetaModMain.MOD_ID,
+        KEYSTONE = Registry.register(Registry.ITEM, new Identifier(ZetaModMain.MOD_ID,
                 "KEYSTONE".toLowerCase(Locale.ROOT)
         ), new KeystoneItem(new FabricItemSettings().group(ItemGroup.MISC)));
     }
