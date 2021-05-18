f3b3e5d3dc5d401325f3ac6e538c307e838cfebe
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-05-18 13:32:46 -0400

    Builds without errors now!

--- src/main/java/mixins/supercoder/MixinNoiseSamplingConfig.java
+++ src/main/java/mixins/supercoder/MixinNoiseSamplingConfig.java
@@ -10,7 +10,7 @@ import net.minecraft.world.gen.chunk.NoiseSamplingConfig;
 @Mixin(NoiseSamplingConfig.class)
 public class MixinNoiseSamplingConfig {
 	@SuppressWarnings("ALL")
-	@Redirect(method = "<clinit>", at = @At(target = "Lcom/mojang/serialization/Codec;doubleRange(DD)Lcom/mojang/serialization/Codec;", value = "INVOKE"))
+	@Redirect(method = "<clinit>", at = @At(target = "Lnet/minecraft/world/gen/chunk/NoiseSamplingConfig;CODEC_RANGE:Lcom/mojang/serialization/Codec;", value = "INVOKE"))
 	private static Codec<Double> fixRange(double min, double max) {
 		return Codec.doubleRange(min, 10000000); // changes it to be 0.001 to 10 million instead of 1 thousand
 	}
