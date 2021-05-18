1cf76f70fc5b3819c1a6268c50dbae27b449b873
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-05-18 13:18:43 -0400

    Updated shard mixins

--- src/main/java/mixins/supercoder/MixinNoiseSamplingConfig.java
+++ src/main/java/mixins/supercoder/MixinNoiseSamplingConfig.java
@@ -9,6 +9,7 @@ import net.minecraft.world.gen.chunk.NoiseSamplingConfig;
 
 @Mixin(NoiseSamplingConfig.class)
 public class MixinNoiseSamplingConfig {
+	@SuppressWarnings("UnresolvedMixinReference")
 	@Redirect(method = "<clinit>", at = @At(target = "Lcom/mojang/serialization/Codec;doubleRange(DD)Lcom/mojang/serialization/Codec;", value = "INVOKE"))
 	private static Codec<Double> fixRange(double min, double max) {
 		return Codec.doubleRange(min, 10000000); // changes it to be 0.001 to 10 million instead of 1 thousand
--- src/main/java/zeta/zetamod/mod/ZetaMod.java
+++ src/main/java/zeta/zetamod/mod/ZetaMod.java
@@ -34,7 +34,7 @@ public class ZetaMod implements ModInitializer {
 	public static final Integer MOD_BF = 0;
 	public static boolean MOD_DEV = true;
 	public static String MOD_DEV_V =
-			"34"
+			"35"
 			//+ "."
 			//+ "2"
 			;
