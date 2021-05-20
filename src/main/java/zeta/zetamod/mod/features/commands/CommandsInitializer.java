1a7dce3ed265d3fd6007d7e2ef6c6444e5a9668b
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-05-20 13:08:35 -0400

    *demon noises*

--- src/main/java/zeta/zetamod/mod/features/commands/CommandsInitializer.java
+++ src/main/java/zeta/zetamod/mod/features/commands/CommandsInitializer.java
@@ -1,9 +1,11 @@
 package zeta.zetamod.mod.features.commands;
 
 
+import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.SharedConstants;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import zeta.zetamod.mod.ZetaMod;
 import zeta.zetamod.mod.managers.GeneralManager;
 //Static imports
@@ -17,48 +19,13 @@ import static net.minecraft.server.command.CommandManager.literal;
 import static net.minecraft.server.command.CommandManager.argument;
 // Import everything
 import static net.minecraft.server.command.CommandManager.*;
-
+import static zeta.zetamod.mod.managers.GeneralManager.getConfig;
 @SuppressWarnings("ALL")
 public class CommandsInitializer {
     public void initCommands() {
-        /**
-         * FarLands Section
-         */
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-            dispatcher.register(literal("farlands")
-                    .executes(context -> {
-                        boolean FLC = GeneralManager.getConfig().farLandsEnabled.getValue();
-                        context.getSource().sendFeedback(new LiteralText("FarLands " + getEnabledOrDisabled(!FLC)), true);
-                        GeneralManager.getConfig().farLandsEnabled.setValue(!FLC);
-                        return 1;
-                    }
-            ));
-            dispatcher.register(literal("farlands")
-                .then(literal("shatter")
-                    .executes(context -> {
-                        boolean SLC = GeneralManager.getConfig().shardFarLands.getValue();
-                        if (SLC) {
-                            context.getSource().sendFeedback(new LiteralText("Shattered farlands disabled"), true);
-                        } else {
-                            context.getSource().sendFeedback(new LiteralText("Shattered farlands enabled, teleport to x = 784444 or x/z 196111 to see them!"), true);
-                        }
-                        GeneralManager.getConfig().shardFarLands.setValue(!SLC);
-                        return 2;
-                    })
-            ));
-            dispatcher.register(literal("farlands")
-                    .then(literal("get")
-                        .executes(context -> {
-                            context.getSource().sendFeedback(new LiteralText(
-                                    "FarLands currently " + getEnabledOrDisabled(GeneralManager.getConfig().farLandsEnabled.getValue())
-                                    + "\nShattered FarLands currently " +getEnabledOrDisabled(GeneralManager.getConfig().shardFarLands.getValue())
-                            ), true);
-                            return 0;
-                        })
-            ));
-
             /**
-             * ZetaMod Section
+             * /
              */
             dispatcher.register(literal("zetamod").then(literal("version").executes(
                     context -> {
@@ -66,7 +33,45 @@ public class CommandsInitializer {
                                 ,true);
                         return 1;
                     }
-            )));
+            )).then(literal("farlands").executes(
+                    context -> {
+                        boolean FLC = GeneralManager.getConfig().farLandsEnabled.getValue();
+                        context.getSource().sendFeedback(new LiteralText("FarLands " + getEnabledOrDisabled(!FLC)), true);
+                        GeneralManager.getConfig().farLandsEnabled.setValue(!FLC);
+                        return 1;
+                    }).then(literal("shatter").executes(
+                            context -> {
+                                boolean SLC = GeneralManager.getConfig().shardFarLands.getValue();
+                                if (SLC) {
+                                    context.getSource().sendFeedback(new LiteralText("Shattered farlands disabled"), true);
+                                } else {
+                                    context.getSource().sendFeedback(new LiteralText("Shattered farlands enabled, teleport to x = 784444 or x/z 196111 to see them!"), true);
+                                        }
+                                GeneralManager.getConfig().shardFarLands.setValue(!SLC);
+                                return 2;
+                            })
+                    .then(literal("get").executes(
+                            context -> {
+                                context.getSource().sendFeedback(new LiteralText(
+                                        "FarLands currently " + getEnabledOrDisabled(GeneralManager.getConfig().farLandsEnabled.getValue())
+                                                + "\nShattered FarLands currently " +getEnabledOrDisabled(GeneralManager.getConfig().shardFarLands.getValue())
+                                ), true);
+                                return 0;
+                            })
+                    )))
+                    .then(
+                            literal("setCoordinateScale".toLowerCase()).
+                                    executes(
+                                    context -> {
+                                        double coorindateScale = GeneralManager.getConfig().coordinateScale.getValue();
+                                        context.getSource().sendError(Text.of("Still wip!"));
+                                        //getConfig().coordinateScale.setValue(StringArgumentType.getString(context, ""))
+                                        //return 1;
+                                        return -1;
+                                    }
+                            )
+                    )
+            );
         });
     }
     public static String getEnabledOrDisabled(boolean bool) {
