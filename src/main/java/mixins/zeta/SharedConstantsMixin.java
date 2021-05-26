583b580faf1944c678a553d419efc1ea1cf93a43
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-05-26 08:35:57 -0400

    Revert "pushing unfinished build because intelij is being conc"
    This reverts commit a1fa4860a3e259b2764a6f3f502a657d98029a6a.

--- src/main/java/mixins/zeta/SharedConstantsMixin.java
+++ src/main/java/mixins/zeta/SharedConstantsMixin.java
@@ -9,12 +9,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import zeta.zetamod.mod.managers.ConfigManager;
 
 @Mixin(SharedConstants.class)
 public class SharedConstantsMixin {
-    @Redirect(method = "<clinit>", at = @At(target = "Lnet/minecraft/SharedConstants;VERSION_NAME:Ljava/lang/String;", value = "INVOKE"))
-    private static String fixVersion(String version) {
-        return ConfigManager.getConfig().fakeVersion.getValue();
-    }
+    /*@Mutable
+    @Shadow @Final @Deprecated public static String VERSION_NAME;
+
+    @Inject(method = "<clinit>", at = @At("INVOKE"))
+    private static void handleConstructor(CallbackInfo ci) {
+        //VERSION_NAME = "1.17";
+    }*/
 }
