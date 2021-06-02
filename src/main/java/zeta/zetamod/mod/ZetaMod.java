a6899388aeca1b23c18cc40ca79eb5f740041b19
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-06-02 13:12:53 -0400

    updated to 1.17-pre4

--- src/main/java/zeta/zetamod/mod/ZetaMod.java
+++ src/main/java/zeta/zetamod/mod/ZetaMod.java
@@ -44,7 +44,7 @@ public class ZetaMod implements ModInitializer {
 	public static final String MOD_VERSION = MOD_MAJOR + "." + MOD_MINOR + "." + MOD_BF;
 	public static boolean MOD_DEV = true;
 	public static final int MOD_DEV_V =
-			612
+			613
 			//+ "."
 			//+ "2"
 			;
