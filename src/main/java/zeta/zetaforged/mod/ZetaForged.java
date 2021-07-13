43f3095e4d59b4567e4de5b6c9c9bcab06a67e0c
MicherGit <40839581+TwelvianZeta@users.noreply.github.com>
2021-07-13 18:24:24 -0400

    1.18 Experimental Snapshot 1 (minus all the worldgen features)

--- src/main/java/zeta/zetaforged/mod/ZetaForged.java
+++ src/main/java/zeta/zetaforged/mod/ZetaForged.java
@@ -1,7 +1,6 @@
 package zeta.zetaforged.mod;
 
 import me.zeroeightsix.fiber.exception.FiberException;
-import net.fabricmc.fabric.api.biome.v1.TheEndBiomes;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.SharedConstants;
@@ -12,8 +11,6 @@ import zeta.zetaforged.mod.managers.commands.CommandsManager;
 import zeta.zetaforged.mod.features.errors.compute.ComputeErrorFunction;
 import zeta.zetaforged.mod.features.items.ConcernedTater;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.biome.v1.OverworldBiomes;
-import net.fabricmc.fabric.api.biome.v1.OverworldClimate;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
@@ -43,9 +40,10 @@ public class ZetaForged implements ModInitializer {
 	//public static final String MOD_VERSION = MOD_MAJOR + "." + MOD_MINOR + "." + MOD_BF;
 	//public static final String MOD_VERSION = "1.0.6";
 	public static String MOD_VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getVersion().toString()
-			+ " snapshot 21w28b";
+			//+ " snapshot 21w28b"
+			;
 	public static boolean MOD_DEV = true;
-	public static final int MOD_DEV_V = 302;
+	public static final int MOD_DEV_V = 1000;
 	public static String getModVersion() {
 		return MOD_VERSION;
 	}
@@ -90,9 +88,13 @@ public class ZetaForged implements ModInitializer {
 			}
 		}
 	}
+	public static final Block GIGACONCERN_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).hardness(8.0f));
+	public static final int MILESTONE_VERSION = 14;
 	@Override
 	public void onInitialize() {
 		log2("Initializing ZetaForged");
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "gigaconcern_block"), GIGACONCERN_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "gigaconcern_block"), new BlockItem(GIGACONCERN_BLOCK, new Item.Settings().group(ZetaForged.ZETAFORGED_ITEMS)));
 
 		//TrinaryHash.checkHash();
 		log(Level.INFO, "Initializing config");
@@ -120,13 +122,14 @@ public class ZetaForged implements ModInitializer {
 			MOD_VERSION_D = MOD_VERSION_D + "." + MOD_DEV_V;
 			log(Level.INFO, "Mod build is " + MOD_VERSION + " development version " + MOD_DEV_V);
 			log(LV, "Milestone build" + " " +
-					12
-					+ " beta!"
+					MILESTONE_VERSION
+					//+ " beta!"
+					+ " early access"
 			);
 
 		} else {
 			log2("Mod build is " + MOD_VERSION + ' ' + PHASE + " build " + MOD_DEV_V);
-			log2("Milestone 12" +
+			log2("Milestone " + MILESTONE_VERSION +
 					//" beta" +
 					"!");
 		}
@@ -137,13 +140,6 @@ public class ZetaForged implements ModInitializer {
 		log(LV,"API version " + api.APIVersionGet());
 		log(Level.INFO, "Loading!");
 		log(Level.INFO, "Adding biomes");
-		OverworldBiomes.addContinentalBiome(BiomeKeys.LUSH_CAVES, OverworldClimate.TEMPERATE, 0.25D);
-		OverworldBiomes.addContinentalBiome(BiomeKeys.DRIPSTONE_CAVES, OverworldClimate.COOL, 0.25D);
-		//OverworldBiomes.addContinentalBiome(BiomeKeys.NETHER_WASTES, OverworldClimate.DRY, 0.0625D);
-		//TheEndBiomes.addHighlandsBiome(BiomeKeys.LUSH_CAVES, 0.0625d);
-		TheEndBiomes.addMainIslandBiome(BiomeKeys.LUSH_CAVES, 0.0625d);
-		TheEndBiomes.addSmallIslandsBiome(BiomeKeys.LUSH_CAVES, 0.25d);
-
 		log(Level.INFO, "Adding concerning items!");
 		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "concern_block"), CONCERN_BLOCK);
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "concern_block"), new BlockItem(CONCERN_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
@@ -154,9 +150,6 @@ public class ZetaForged implements ModInitializer {
 		//ZetaForged.log(Level.INFO, "Patching Farlands!");
 
 		//log2("Adding a very concern easter egg");
-		OverworldBiomes.addContinentalBiome(BiomeKeys.THE_VOID, OverworldClimate.DRY,
-				0.5D);
-
 
 		BiomesInitializer.initializeBiomes();
 		log(Level.INFO, "Registering extra items...");
--- src/main/java/zeta/zetaforged/mod/features/biomes/BiomesInitializer.java
+++ src/main/java/zeta/zetaforged/mod/features/biomes/BiomesInitializer.java
@@ -3,8 +3,8 @@ package zeta.zetaforged.mod.features.biomes;
 public class BiomesInitializer {
     public static void initializeBiomes() {
         // Register Biomes
-        ObsidianBiome.register();
-        ConcerningBiome.register();
-        HellscapeMountainsBiome.register();
+        //ObsidianBiome.register();
+        //ConcerningBiome.register();
+        //HellscapeMountainsBiome.register();
     }
 }
