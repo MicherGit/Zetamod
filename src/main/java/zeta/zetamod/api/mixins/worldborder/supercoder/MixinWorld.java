a16a22e151221d4e36032308fdbe9c2506e00919
TwelvianZeta <40839581+TwelvianZeta@users.noreply.github.com>
2021-07-19 16:43:04 -0400

    fix worldborder mixins on 1.0.x (#11 fix) (#13)
    * Update gradle.properties
    
    Bump ver
    
    * Update MixinServerPlayNetworkHandler.java
    
    Patch 30M bug
    
    * update worldborder mixins (on pc)

--- src/main/java/zeta/zetamod/api/mixins/worldborder/supercoder/MixinWorld.java
+++ src/main/java/zeta/zetamod/api/mixins/worldborder/supercoder/MixinWorld.java
@@ -1,5 +1,8 @@
 package zeta.zetamod.api.mixins.worldborder.supercoder;
 
+import net.minecraft.util.math.ChunkSectionPos;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -8,7 +11,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 @Mixin(World.class)
-public class MixinWorld {
+public abstract class MixinWorld implements WorldAccess {
 
 	/**
 	 * @author SuperCoder79
@@ -28,5 +31,24 @@ public class MixinWorld {
 	@Overwrite @Final
 	public static boolean isInvalidVertically(int y) {
 		return y < Integer.MIN_VALUE || y >= Integer.MAX_VALUE;
+	}
+
+	/**
+	 * @author
+	 */
+	@Overwrite
+	public int getTopY(Heightmap.Type heightmap, int x, int z) {
+		int k;
+		if (x >= Integer.MIN_VALUE && z >= Integer.MIN_VALUE && x < Integer.MAX_VALUE && z < Integer.MAX_VALUE) {
+			if (this.isChunkLoaded(ChunkSectionPos.getSectionCoord(x), ChunkSectionPos.getSectionCoord(z))) {
+				k = this.getChunk(ChunkSectionPos.getSectionCoord(x), ChunkSectionPos.getSectionCoord(z)).sampleHeightmap(heightmap, x & 15, z & 15) + 1;
+			} else {
+				k = this.getBottomY();
+			}
+		} else {
+			k = this.getSeaLevel() + 1;
+		}
+
+		return k;
 	}
 }
